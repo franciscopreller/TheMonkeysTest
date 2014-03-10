4dc0667002b96171681baca16f27bba22a163deb
franciscopreller <francisco.preller@gmail.com>
2014-03-10 17:27:58 +1100

    Implement MonkeyJS - initial config

--- build/public/tests/config.js
+++ build/public/tests/config.js
@@ -1,41 +1,46 @@
 (function(global) {
     global.monkeytestjs = {
-        "facebookId": "000000000000000",
+        
+        // "facebookId": "000000000000000",
 
         "local": {
-            "env": [/DEV URL OR PART OF/]
+            "env": "localhost"
         },
-        "stage": {
-            "env": [/STAGE URL OR PART OF/],
-            "facebookId": "222222222222222"
-        },
-        "beta": {
-            "env": [/BETA URL OR PART OF/],
-            "facebookId": "33333333333333333"
-        },
-        "production": {
-            "env": [/PRODUCTION URL OR PART OF/],
-            "facebookId": "4444444444444444444"
-        },
+
+        // Comment out unnecessary development environments for MonkeyTest
+        // Francisco Preller: 10/03/2014
+
+        // "stage": {
+        //     "env": [/STAGE URL OR PART OF/],
+        //     "facebookId": "222222222222222"
+        // },
+        // "beta": {
+        //     "env": [/BETA URL OR PART OF/],
+        //     "facebookId": "33333333333333333"
+        // },
+        // "production": {
+        //     "env": [/PRODUCTION URL OR PART OF/],
+        //     "facebookId": "4444444444444444444"
+        // },
         "testsDir": "mytests",
         "globalTests": [
-            "global/not_server_error.js",
+            //"global/not_server_error.js",
             "global/is_html_w3c_valid.js",
-            "global/has_utf8_metatag.js",
-            "global/has_google_analytics.js"
+            //"global/has_utf8_metatag.js",
+            //"global/has_google_analytics.js"
         ],
         "pages": [
             {
                 "url": "../"
             },
-            {
-                "url": "core/demo/index.html",
-                "tests": [ "page/has_facebook_appid.js" ]
-            },
-            {
-                "url": "core/demo/index.html?pretendIsAnotherPage=true",
-                "tests": [ "page/has_facebook_appid.js", "page/demo_page_test.js" ]
-            }
+            // {
+            //     "url": "core/demo/index.html",
+            //     "tests": [ "page/has_facebook_appid.js" ]
+            // },
+            // {
+            //     "url": "core/demo/index.html?pretendIsAnotherPage=true",
+            //     "tests": [ "page/has_facebook_appid.js", "page/demo_page_test.js" ]
+            // }
         ],
         "proxyUrl": "core/proxy.php?mode=native&url=<%= url %>",
         "loadSources": true
