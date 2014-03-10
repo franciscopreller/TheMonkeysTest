b52ab71f7c0ba485cb635579931db88ab7cfcb59
franciscopreller <francisco.preller@gmail.com>
2014-03-10 14:36:29 +1100

    Added enquiry controller and view.

--- build/app/views/enquiry.blade.php
+++ build/app/views/enquiry.blade.php
@@ -2,12 +2,82 @@
 
 @section('body')
 
-	{{ Form::open(array('url' => '', 'method' => 'post')) }}
-	<section class="row">
-		<div class="small-12 medium-8 medium-offset-2">
-			
-		</div>
-	</section>
+	{{ Form::open(array('url' => '', 'method' => 'post', 'class' => 'enquiry-form')) }}
+
+	<div class="form-section-wrapper">
+		<!-- First name -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('firstName', 'First name', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::text('firstName', Input::old('firstName')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Last name -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('surname', 'Surname', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::text('surname', Input::old('surname')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Email address -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('email', 'Email address', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::text('email', Input::old('email')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Daytime contact number -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('contactNumber', 'Daytime contact number', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::text('contactNumber', Input::old('contactNumber')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<div class="form-section-wrapper">
+		<!-- First name -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('firstName', 'First name', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::text('firstName', Input::old('firstName')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+
 	{{ Form::close() }}
 
 @stop
