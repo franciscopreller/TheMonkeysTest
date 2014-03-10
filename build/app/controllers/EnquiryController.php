0ff5fd322086969a226354c1a66b6722d8830c95
franciscopreller <francisco.preller@gmail.com>
2014-03-10 15:05:15 +1100

    Enquiry form & styling - Added POST RESTful state method

--- build/app/controllers/EnquiryController.php
+++ build/app/controllers/EnquiryController.php
@@ -7,7 +7,33 @@ class EnquiryController extends BaseController {
 
 	public function getIndex()
 	{
-		return View::make('enquiry');
+		$view = View::make('enquiry');
+
+		// Initialize states
+		$view->states = array(
+			'ACT' => 'Australian Capital Territory',
+			'NSW' => 'New South Wales',
+			'NT'  => 'Northern Territory',
+			'QLD' => 'Queensland',
+			'SA'  => 'South Australia',
+			'TAS' => 'Tasmania',
+			'VIC' => 'Victoria',
+			'WA'  => 'Western Australia'
+		);
+
+		// Initialize enquiry types
+		$view->enquiryTypes = array(
+			'General enquiry',
+			'Product feedback or enquiry',
+			'Product complaint'
+		);
+
+		return $view;
+	}
+
+	public function postIndex()
+	{
+
 	}
 
 }
--- build/app/views/enquiry.blade.php
+++ build/app/views/enquiry.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
 
-	{{ Form::open(array('url' => '', 'method' => 'post', 'class' => 'enquiry-form')) }}
+	{{ Form::open(array('url' => 'enquiry', 'method' => 'post', 'class' => 'enquiry-form')) }}
 
 	<div class="form-section-wrapper">
 		<!-- First name -->
@@ -63,17 +63,96 @@
 	</div>
 
 	<div class="form-section-wrapper">
-		<!-- First name -->
+		<!-- Address -->
 		<section class="row">
 			<div class="small-12 medium-8 medium-offset-2 columns">
 				<div class="row">
 					<div class="medium-4 columns">
-						{{ Form::label('firstName', 'First name', array('class' => 'inline small-text-left medium-text-right')) }}
+						{{ Form::label('address', 'Address', array('class' => 'inline small-text-left medium-text-right')) }}
 					</div>
 					<div class="medium-8 columns">
-						{{ Form::text('firstName', Input::old('firstName')) }}
+						{{ Form::text('address', Input::old('address')) }}
 					</div>
 				</div>
+			</div>
+		</section>
+
+		<!-- Suburb -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('suburb', 'Suburb', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::text('suburb', Input::old('suburb')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- State -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('state', 'State', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::select('state', $states, Input::old('state')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Postcode -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('postcode', 'Postcode', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::text('postcode', Input::old('postcode')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<div class="form-section-wrapper">
+		<!-- Enquiry type -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('enquiryType', 'Enquiry type', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::select('enquiryType', $enquiryTypes, Input::old('enquiryType')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Enquiry -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns">
+				<div class="row">
+					<div class="medium-4 columns">
+						{{ Form::label('enquiry', 'Enquiry', array('class' => 'inline small-text-left medium-text-right')) }}
+					</div>
+					<div class="medium-8 columns">
+						{{ Form::textarea('enquiry', Input::old('enquiry')) }}
+					</div>
+				</div>
+			</div>
+		</section>
+	
+		<!-- Submit -->
+		<section class="row">
+			<div class="small-12 medium-8 medium-offset-2 columns text-right">
+				{{ Form::submit('SUBMIT >') }}
 			</div>
 		</section>
 	</div>
